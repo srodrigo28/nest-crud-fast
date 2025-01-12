44e80346e219945f1bd287a0d41eeb8f958ecefa
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-01-11 22:20:40 -0300

    updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { ProdutoModule } from './produto/produto.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DbModule, ProdutoModule],
+  imports: [DbModule, ProdutoModule, UsersModule],
   controllers: [],
   providers: [],
 })
